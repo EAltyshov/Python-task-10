49b8ce89ae2aa484b60fb33a095021eff6521267
EAltyshov <120562993+EAltyshov@users.noreply.github.com>
2023-01-25 20:39:26 +0300

    Так и не получилось справиться с ошибкой

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
-from fastapi import FastAPI, Query
-from schemas import Request, Word
+from fastapi import FastAPI, Query, UploadFile, HTTPException, File
 from typing import List
-import collections
+import collections, os, os.path
+from fastapi.responses import FileResponse
+import schemas
+
 
 app = FastAPI()
 
-@app.post('/request')
-def post_name(list1: List[str] = Query([])):
+@app.post('/filter/case_sensitive')
+def family(list1: List[str] = Query([])):
     list2 = [item for item, count in collections.Counter(list1).items() if count > 1]
     list3 = [x.lower() for x in list1]
     list4 = [x.lower() for x in list2]
     result = list(set(list3) ^ set(list4))
     return result
+@app.post("/upload/{filename}")
+def accumulate_files(files: list[UploadFile], filename: str):
+    error_files = []
+    for file in files:
+        if not file.filename.endswith((".csv", ".json")):
+            error_files.append(file.filename)
+    if error_files:
+        raise HTTPException(status_code=415, detail=error_files)
+    schemas.accum_files(files, filename)
+    return {"filename": filename}
+
+
+
+@app.post("/load/{filename}")
+def load_file(filename: str):
+    path = os.path.join('data/', filename)
+    if os.path.isfile(path):
+        return FileResponse(path)
+    else:
+        raise HTTPException(status_code=404, detail="file not found: " + filename)
+
+@app.post("/upload/<file_name>")
+def upload_file(file: UploadFile = File(...)):
+    error_files = "Wrong format: .csv, .json"
+    if file.filename.endswith((".csv", ".json")):
+        with open(f'{file.filename}', 'wb') as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        return {"file": file}
+    else:
+        raise HTTPException(status_code=415, detail=error_files)
--- schemas.py
+++ schemas.py
@@ -1,9 +1,24 @@
-from typing import List
-from pydantic import BaseModel
-from datetime import date
+import os, os.path
+from fastapi import UploadFile
+import pandas as pd
+os.environ["DATA_DIR"] = 'C:\\Users\\EAltyshov\\PycharmProjects\\Python-task-10'
 
-class Word(BaseModel):
-    name: str
 
-class Request(BaseModel):
-    words: List[Word]
+def accum_files(files: list[UploadFile], result_filename: str):
+    li = []
+    for file in files:
+        if file.filename.endswith(".csv"):
+            df = pd.read_csv(file.file, sep=';')
+            li.append(df)
+        elif file.filename.endswith(".json"):
+            df = pd.read_json(file.file)
+            li.append(df)
+    concat_df = pd.concat(li, ignore_index=True)
+
+    path = os.path.join('data/', result_filename)
+    if os.path.isfile(path):
+        with open(path, "r") as original_file:
+            original_df = pd.read_csv(original_file, sep=';')
+            concat_df = pd.concat([original_df, concat_df], ignore_index=True)
+
+    concat_df.to_csv(os.path.join('data/', result_filename), sep=';', index=False)
